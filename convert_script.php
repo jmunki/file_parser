dd98e05f02ae90237062a9d55f00d6bf27965a2a
jmunki <jackmonk87@hotmail.com>
2015-12-21 22:42:42 +0000

    changed personal data and added todo list

--- convert_script.php
+++ convert_script.php
@@ -4,21 +4,22 @@ require('utils/file_parser/TypeConverter.php');
 use utils\file_parser\TypeConverter as Converter;
 
 /**
- * TODO Deal with CLI arguments and get input data
+ * TODO
+ * Check for input filename flag, read file into variable
+ * Check for input from STDIN
+ * Check for input type format
+ *
+ * Check for output type format
+ * Check for output filename flag, save results to file
  */
 
 
 $mix_input_test = 'Name,Address_1,Address_2,Address_3,Address_4,Address_Postcode,Contact_Home,Contact_Work
-Jack Monk,1503 City Heights,Victoria Bridge Street,Salford, Greater Manchester,M35AS, , 0161 234234,
-Another Person, 608 Marsden House, Marsden Road, Bolton, Greater Manchester,BL12JT, 01618343844,';
+David Corbyn,1203 City Heights,Victoria Bridge Street,Salford, Greater Manchester,M35AS, , 0161 234234,
+Jeremy Cameron, 408 Marsden House, Marsden Road, Bolton, Greater Manchester,BL12JT, 01613343844,';
 $obj_parser = new Converter();
-$mix_output = $obj_parser->convert($mix_input_test, Converter::TYPE_CSV, Converter::TYPE_XML);
-//$mix_output = $obj_parser->convert($mix_output, Converter::TYPE_JSON);
+$mix_output = $obj_parser->convert($mix_input_test, Converter::TYPE_CSV);
 
-
-/**
- * TODO Deal with output based on CLI arguments
- */
 echo print_r($mix_output, true);
 
 
