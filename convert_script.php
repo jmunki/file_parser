c2eb99dc99ff8c1b01803f2c10bc79480ddf9be4
jmunki <jackmonk87@hotmail.com>
2015-12-23 00:22:06 +0000

    can now specify types in any case and added better naming for some variables

--- convert_script.php
+++ convert_script.php
@@ -17,29 +17,30 @@ $arr_valid_input_types = [
 ];
 $arr_valid_output_types = [
     "XML" => Converter::TYPE_XML,
-    "JSON" => Converter::TYPE_JSON
+    "JSON" => Converter::TYPE_JSON,
+    "CSV" => Converter::TYPE_CSV
 ];
 
-$str_input_type = "";
-$str_output_type = "";
+$str_requested_input_type = "";
+$str_requested_output_type = "";
 $str_example_usage = "Example script usage:\n$[script_name] [input_type] [output_type] [input_file_name] [output_file_name]";
-$str_input_file = "php://stdin";
-$str_output_file = "php://stdout";
+$str_requested_input_file = "php://stdin";
+$str_requested_output_file = "php://stdout";
 
 // Check input/output types are valid
-if (! isset($argv[1]) || ! array_key_exists($argv[1], $arr_valid_input_types)) {
+if (! isset($argv[1]) || ! array_key_exists(strtoupper($argv[1]), $arr_valid_input_types)) {
     echo "Please specify a valid input type: " . implode(", ", array_keys($arr_valid_input_types)) . "\n";
     echo "$str_example_usage\n\n";
     exit;
 }
-$str_input_type = $arr_valid_input_types[$argv[1]];
+$str_requested_input_type = $arr_valid_input_types[strtoupper($argv[1])];
 
-if (! isset($argv[2]) || ! array_key_exists($argv[2], $arr_valid_output_types)) {
+if (! isset($argv[2]) || ! array_key_exists(strtoupper($argv[2]), $arr_valid_output_types)) {
     echo "Please specify a valid output type: " . implode(", ", array_keys($arr_valid_output_types)) . "\n";
     echo "$str_example_usage\n\n";
     exit;
 }
-$str_output_type = $arr_valid_output_types[$argv[2]];
+$str_requested_output_type = $arr_valid_output_types[strtoupper($argv[2])];
 
 //  Check input/output file names
 if (! isset($argv[3]) || empty($argv[3])) {
@@ -48,7 +49,7 @@ if (! isset($argv[3]) || empty($argv[3])) {
     exit;
 }
 if($argv[3] !== "-"){
-    $str_input_file = $argv[3];
+    $str_requested_input_file = $argv[3];
 }
 
 if (! isset($argv[4]) || empty($argv[4])) {
@@ -57,20 +58,20 @@ if (! isset($argv[4]) || empty($argv[4])) {
     exit;
 }
 if($argv[4] !== "-"){
-    $str_output_file = $argv[4];
+    $str_requested_output_file = $argv[4];
 }
 
 /**
  *  Input
  */
-$str_input_content = file_get_contents($str_input_file);
+$str_input_content = file_get_contents($str_requested_input_file);
 /**
  *  Convert
  */
 $obj_parser = new Converter();
-$mix_output = $obj_parser->convert($str_input_content, $str_input_type, $str_output_type);
+$mix_output = $obj_parser->convert($str_input_content, $str_requested_input_type, $str_requested_output_type);
 /**
  *  Output
  */
-file_put_contents($str_output_file, $mix_output);
+file_put_contents($str_requested_output_file, $mix_output);
 
